c387ebf01e3515952d888bced7ae313423901259
Braeden Ford <bford0002@gmail.com>
2022-08-25 12:20:08 -0500

    dynamic rendering projects/links working

--- Portfolio/src/app/projects/projects.component.ts
+++ Portfolio/src/app/projects/projects.component.ts
@@ -6,8 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+  projects = [
+    {
+      name: 'Fern Herm',
+      description: 'A pet/plant sitting app, allowing users to create and accept jobs based on their needs.',
+      link: 'github.com/fern-herm/pet-plant-sitting'
+    },
+    {
+      name: 'Big Easy',
+      description: 'A casino app designed to replicate different casino games to give the user the rush of gambling.',
+      link: 'github.com/TheYesMen/Iben-sCasino'
+    },
+    {
+      name: 'Smarter Than',
+      description: 'A trivia app that is designed to track your progress as you play through multiple rounds.',
+      link: 'github.com/TheYesMen/Are-you-smarter-than-an-OS-student-'
+    }
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
