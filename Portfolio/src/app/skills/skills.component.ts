7afdf6c00ca4ba534644c5f052b247451b1a199d
Braeden Ford <bford0002@gmail.com>
2022-09-01 15:26:38 -0500

    skills svg's up/linkedin and github links on navbar

--- Portfolio/src/app/skills/skills.component.ts
+++ Portfolio/src/app/skills/skills.component.ts
@@ -8,17 +8,54 @@ import { Component, OnInit } from '@angular/core';
 export class SkillsComponent implements OnInit {
 
   skills = [
-    'React', 'Angular', 'Bootstrap', 'Material-UI', 'CSS', 
-    'Node.js', 'Express', 'MongoDB', 'PostgreSQL', 'Javascript', 'TypeScript', 'React Redux', 
-    'UX', 'UI', 'Axios', 'Webpack', 'Mongoose', 'Sequelize', 'MySQL', 'AWS EC2', 'Passport', 
-    'Cloudinary', 'Mapbox', 'Git', 'Scrum', 'Agile'
+    {
+      skill: 'React',
+      logo: '../../assets/img/reactjs-icon.svg'
+    },
+    {
+      skill: 'Angular',
+      logo: '../../assets/img/angular.svg'
+    },
+    {
+      skill: 'Bootstrap',
+      logo: '../../assets/img/bootstrap-4.svg'
+    },
+    {
+      skill: 'Material-UI',
+      logo: '../../assets/img/material-ui-1.svg'
+    },
+    {
+      skill: 'CSS',
+      logo: '../../assets/img/css-3-logo-svgrepo-com.svg'
+    },
+    {
+      skill: 'Node.js',
+      logo: '../../assets/img/nodejs-2.svg'
+    },
+    {
+      skill: 'Express',
+      logo: '../../assets/img/express-js-icon.svg'
+    },
+    {
+      skill: 'MongoDB',
+      logo: '../../assets/img/mongodb.svg'
+    },
+    {
+      skill: 'PostgreSQL',
+      logo: '../../assets/img/postgresql-vertical.svg'
+    },
+    // 'React', 'Angular', 'Bootstrap', 'Material-UI', 'CSS', 
+    // 'Node.js', 'Express', 'MongoDB', 'PostgreSQL', 'Javascript', 'TypeScript', 'React Redux', 
+    // 'UX', 'UI', 'Axios', 'Webpack', 'Mongoose', 'Sequelize', 'MySQL', 'AWS EC2', 'Passport', 
+    // 'Cloudinary', 'Mapbox', 'Git', 'Scrum', 'Agile'
   ]
 
   topSkills = this.skills.slice(0, 9);
 
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
